1674b288fd0fee41dff45d10d12aa2d933d0465b
Erika Oliveira <139921490+erikaoliveiracorrea@users.noreply.github.com>
2025-09-10 23:42:20 +0000

    corrigindo desafio novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,33 +5,31 @@ int main()
     // Tabuleiro 10x10 inicializado com 0 (água)
     int tabuleiro[10][10] = {0};
 
-   
+    // Letras para as colunas (A até J)
     char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-    // Tamanho fixo dos navios
-    int tamanhoNavio = 3;
+    // quantidade de casas dos navios
+    int casaDoNavio = 3;
 
-    // Coordenadas iniciais dos navios
-    // linha = índice da linha (0 = primeira linha)
-    int linhaNavio1 = 2, colunaNavio1 = 1; // Navio 1 (horizontal)
-    int linhaNavio2 = 4, colunaNavio2 = 2; // Navio 2 (vertical)
-
-    // Posiciona o Navio 1 (horizontal)
-    for (int i = 0; i < tamanhoNavio; i++)
+    // linhas Horizontal
+    int linhaHorizontal = 2;  // linha 3 índice 2
+    int colunaHorizontal = 0; // começa na coluna A índice 0
+    for (int i = 0; i < casaDoNavio; i++)
     {
-        tabuleiro[linhaNavio1][colunaNavio1 + i] = 3;
+        tabuleiro[linhaHorizontal][colunaHorizontal + i] = 3;
     }
 
-    // Posiciona o Navio 2 (vertical)
-    for (int i = 0; i < tamanhoNavio; i++)
+    // linhas vertiacal 5, 6 e 7 ===
+    int linhaVertical = 4;  // linha 5 índice 4
+    int colunaVertical = 2; // começa na coluna C índice 2
+    for (int i = 0; i < casaDoNavio; i++)
     {
-        tabuleiro[linhaNavio2 + i][colunaNavio2] = 3;
+        tabuleiro[linhaVertical + i][colunaVertical] = 3;
     }
 
-  
     printf("TABULEIRO BATALHA NAVAL\n\n");
 
-    //colunas (A até J)
+    // colunas A até J
     printf("   ");
     for (int c = 0; c < 10; c++)
     {
@@ -39,13 +37,13 @@ int main()
     }
     printf("\n");
 
-    // Exibição das linhas com números (1 até 10)
+    // Exibição das linhas com números 1 até 10
     for (int i = 0; i < 10; i++)
     {
         printf("%-2d ", i + 1); // número da linha
         for (int j = 0; j < 10; j++)
         {
-            printf("%d ", tabuleiro[i][j]);
+            printf("%d ", tabuleiro[i][j]); // tudo 0 (água)
         }
         printf("\n");
     }
